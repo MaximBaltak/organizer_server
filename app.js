67332980c3ab4d1aa3575469fccae9f291d9858e
MaximBaltak <maksimbaltak@yandex.ru>
2021-12-15 18:25:31 +0200

    fix url

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const fs =require('fs')
 const express=require('express')
 const app=express()
-const server=require('http').createServer(app)
 const port=process.env.PORT||3000
 app.get('/',async (req,res)=>{
     res.send('<h1>Welcome to server of organizer:)</h1>')
 })
 
-server.listen(port,()=>{
+app.listen(port,()=>{
     console.log('server start')
 })

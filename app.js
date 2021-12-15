9c977714a08dfeb7a269b314addb6ab5139541f2
MaximBaltak <maksimbaltak@yandex.ru>
2021-12-15 18:28:47 +0200

    fix url

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs =require('fs')
 const express=require('express')
 const app=express()
 const port=process.env.PORT||3000
-app.get('/',async (req,res)=>{
+app.get('https://online-organizer.herokuapp.com/', (req,res)=>{
     res.send('<h1>Welcome to server of organizer:)</h1>')
 })
 

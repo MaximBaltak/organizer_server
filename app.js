a580bcb016898a5e3c0fa3dfc93ed312af7a1f2f
MaximBaltak <maksimbaltak@yandex.ru>
2021-12-15 18:33:42 +0200

    fix #5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs =require('fs')
 const express=require('express')
 const app=express()
 const port=process.env.PORT||3000
-app.get('https://online-organizer.herokuapp.com/', (req,res)=>{
+app.get('/', (req,res)=>{
     res.send('<h1>Welcome to server of organizer:)</h1>')
 })
 

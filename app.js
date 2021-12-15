37f197274a86afa6358ac9fde6496020ea48106a
MaximBaltak <maksimbaltak@yandex.ru>
2021-12-15 18:13:55 +0200

    fix #2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app=express()
 const server=require('http').createServer(app)
 const port=process.env.PORT||'3000'
 app.get('/',async (req,res)=>{
-    res.send('<h1>Welcome to server of organizer</h1>')
+    res.send('<h1>Welcome to server of organizer:)</h1>')
 })
 
 server.listen(port,()=>{

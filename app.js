84f5a0d1442670a0404662c8b0facee465c1ab4a
MaximBaltak <maksimbaltak@yandex.ru>
2021-12-15 18:19:00 +0200

    fix port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs =require('fs')
 const express=require('express')
 const app=express()
 const server=require('http').createServer(app)
-const port=process.env.PORT||'3000'
+const port=process.env.PORT||3000
 app.get('/',async (req,res)=>{
     res.send('<h1>Welcome to server of organizer:)</h1>')
 })

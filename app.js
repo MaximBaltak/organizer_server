cf254a66a2e3de0f63f916a629bd4809571802b7
MaximBaltak <maksimbaltak@yandex.ru>
2021-12-15 19:15:50 +0200

    add CORS

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const fs =require('fs')
 const express=require('express')
 const app=express()
 const port=process.env.PORT||3000
+const cors=require('cors')
+const optionsCORS={
+    origin:'https://online-organizer.herokuapp.com'
+
+}
+app.use(cors(optionsCORS))
 app.get('/', (req,res)=>{
     res.send('<h1>Welcome to server of organizer:)</h1>')
 })

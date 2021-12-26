5884777dec915ca9691bda79409a6b6a7ca129a4
MaximBaltak <maksimbaltak@yandex.ru>
2021-12-26 18:52:05 +0200

    add function timer

--- requests/requestTasks.js
+++ requests/requestTasks.js
@@ -1,5 +1,53 @@
 const Task = require('./../database/schemas/Task')
 const checkUser = require('./../middlewares/checkUser')
+const dayFunction=(task) => {
+    let date1 = new Date(task.dateStart).getTime()
+    let date2 = new Date(task.dateEnd).getTime()
+    let t = date2 - date1
+    let day = Math.floor(t / (1000 * 60 * 60 * 24))
+    let color
+    if (day <= 2) {
+        color = 'red'
+    } else if (day > 2 && day <= 5) {
+        color = 'yellow'
+    } else {
+        color = 'green'
+    }
+    let text
+    if (day === 1) {
+        text = `${day} день`
+    } else if (day > 1 && day <= 4) {
+        text = `${day} дня`
+    } else if (day > 4) {
+        text = `${day} дней`
+    } else if (day < 1 && day >= -4) {
+        text = `${day} дня`
+    } else if (day < -4) {
+        text = `${day} дней`
+    } else if (day === 0) {
+        text = `${day} дней`
+    }
+
+    let border
+    let textState
+    let colorState
+    if (day < 0) {
+        border = 'red'
+        textState = 'Просрочена'
+        colorState = 'red'
+    } else {
+        border = 'grey'
+        textState = 'В процессе'
+        colorState = 'yellow'
+    }
+        if (task.day) {
+            task.day.text = text
+            task.day.color = color
+            task.state.text = textState
+            task.state.color = colorState
+            task.border = border
+        }
+}
 
 class RequestTasks {
     async getTasks(req, res) {
@@ -18,7 +66,7 @@ class RequestTasks {
             const tasks = await Task.find({userId})
             if (tasks.length > 0) {
                 Array.from(tasks).forEach(task => {
-                    task._id.toString()
+                    dayFunction(task)
                 })
             }
             return res.status(200).json({tasks: tasks})
